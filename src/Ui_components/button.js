09d3fe12a3727b074298bcacef5a9cbca35b4de9
Zeetahub <zeetaza.center@gmail.com>
2022-01-04 15:33:18 +0700

    1 file modified

--- src/Ui_components/button.js
+++ src/Ui_components/button.js
@@ -1,3 +1,12 @@
 import * as React from 'react';
 import stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+
+
+export default function basicbutton() {
+  return (
+    <Stack spacing={2} direction="row">
+<Button variant="text">Text</Button>
+    </Stack>
+  );
+}
