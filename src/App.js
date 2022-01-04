23f17a956f35ed6da254a7582253f4b9c663d6ce
Zeetahub <zeetaza.center@gmail.com>
2022-01-04 16:29:01 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import Divider from '@mui/material/Divider';
 import { Basicbutton } from './Ui_components/button';
 
 export default function App() {
   return (
     <div>
       <h1>Hello Tiger!</h1>
-      <p>Start editing to see some magic happen :)</p>
-
-      <button type="button">Click</button>
+      <p>Button example from MUI</p>
       <Basicbutton />
+      <br />
+      <Divider />
     </div>
   );
 }
--- src/Ui_components/button.js
+++ src/Ui_components/button.js
@@ -2,6 +2,14 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export function basicbutton() {
-  return <>Test button</>;
+export function Basicbutton() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">text</Button>
+      <Button variant="contained" size="large">
+        clickhere
+      </Button>
+      <Button variant="outlined">geedfds</Button>
+    </Stack>
+  );
 }
