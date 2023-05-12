482d8844bfb6deab65b02a195efc5123c465e0aa
Zeetahub <zeetaza.center@gmail.com>
2023-05-12 10:45:43 +0700

    switch fix

--- src/Ui_components/Switch.js
+++ src/Ui_components/Switch.js
@@ -3,7 +3,7 @@ import Switch from '@mui/material/Switch';
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
-export default function BasicSwitches() {
+export function BasicSwitches() {
     return (
         <div>
             <Switch {...label} defaultChecked />
